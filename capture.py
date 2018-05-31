7a9e00e56113168fbfec72756035deeb498cee78
site0801 <skuroneko0801@gmail.com>
2018-05-31 09:13:20 +0900

    ok

--- capture.py
+++ capture.py
@@ -1,18 +1,18 @@
+#-*- coding:utf-8 -*-
+import sys
 from socket import *
 
-def main():
+def main(interface):
     i = 0
     ETH_P_IP = 0x800
-    interface = "enp0s9"
+    #socket()を作成する
     sock = socket(PF_PACKET, SOCK_RAW, ETH_P_IP)
     sock.bind((interface, ETH_P_IP))
     while True:
         packet = sock.recv(4096)
-        packet_len = len(packet)
-        i += 1
-        
-        src = ":".join(["%02x" % x for x in packet[0:6]])
-        dst = ":".join(["%02x" % x for x in packet[6:12]])
+        packet_len = len(packet
+        dst = ":".join(["%02x" % x for x in packet[0:6]])
+        src = ":".join(["%02x" % x for x in packet[6:12]])
         type = ntohs(ord(packet[12:13]))
         print("src:%s > dst:%s, ethertype:%04x, length:%d" % (src, dst, type, packet_len))
         '''
@@ -23,10 +23,19 @@ def main():
             packet.extend([1, 127])
             fout.write(packet)
         '''
-        print(packet.hex())
+        #packet内にあるバイナリを16進に変換する(wiresharkの下に表示されてるような感じになる。)
+        packet = packet.hex()
+        print(packet)
         print("")
         print("")
-
 
 if __name__ == '__main__':
-    main()
+    argvs = sys.argv
+    argc = len(argvs)
+
+    if argc != 2:
+      print("Please confirm argument")
+      sys.exit()
+    interface = argvs[1]
+    main(interface)
+
